2a93d83e2da47df514f7f65d3730002f8fcb8a3f
Mahmoud Abdelkader <mahmoud@mail.milo.com>
2010-02-02 12:04:50 -0800

    Added transposing of windows C-c s

--- mahmoud.el
+++ mahmoud.el
@@ -116,3 +116,36 @@
 ;; in insert mode, let's just keep it like Vim's insert
 ;; mode
 ;; (setq default-frame-alist '((cursor-type . (bar . 10))))
+
+;; ------------------------------------
+;; hotkeys
+;; ------------------------------------
+
+
+;; ------------------------------------
+;; transpose buffers
+;; ------------------------------------
+(setq swapping-buffer nil)
+(setq swapping-window nil)
+(defun swap-buffers-in-windows ()
+  "Swap buffers between two windows"
+  (interactive)
+  (if (and swapping-window
+           swapping-buffer)
+      (let ((this-buffer (current-buffer))
+            (this-window (selected-window)))
+        (if (and (window-live-p swapping-window)
+                 (buffer-live-p swapping-buffer))
+            (progn (switch-to-buffer swapping-buffer)
+                   (select-window swapping-window)
+                   (switch-to-buffer this-buffer)
+                   (select-window this-window)
+                   (message "Swapped buffers."))
+          (message "Old buffer/window killed.  Aborting."))
+        (setq swapping-buffer nil)
+        (setq swapping-window nil))
+    (progn
+      (setq swapping-buffer (current-buffer))
+      (setq swapping-window (selected-window))
+      (message "Buffer and window marked for swapping."))))
+(global-set-key (kbd "C-c s") 'swap-buffers-in-windows)
