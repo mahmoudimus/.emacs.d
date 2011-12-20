f78f0466805ce401827d7e1898bcfbe3fda5b304
Mahmoud Abdelkader <mabdelkader@gmail.com>
2011-12-20 02:13:41 -0800

    add support python

--- modules/core-programming.el
+++ modules/core-programming.el
@@ -32,5 +32,68 @@
 ;; Boston, MA 02110-1301, USA.
 
 ;;; Code:
+(require 'imenu)
+
+(defun core-ido-goto-symbol (&optional symbol-list)
+  "Refresh imenu and jump to a place in the buffer using Ido."
+  (interactive)
+  (unless (featurep 'imenu)
+    (require 'imenu nil t))
+  (cond
+   ((not symbol-list)
+    (let ((ido-mode ido-mode)
+          (ido-enable-flex-matching
+           (if (boundp 'ido-enable-flex-matching)
+               ido-enable-flex-matching t))
+          name-and-pos symbol-names position)
+      (unless ido-mode
+        (ido-mode 1)
+        (setq ido-enable-flex-matching t))
+      (while (progn
+               (imenu--cleanup)
+               (setq imenu--index-alist nil)
+               (prelude-ido-goto-symbol (imenu--make-index-alist))
+               (setq selected-symbol
+                     (ido-completing-read "Symbol? " symbol-names))
+               (string= (car imenu--rescan-item) selected-symbol)))
+      (unless (and (boundp 'mark-active) mark-active)
+        (push-mark nil t nil))
+      (setq position (cdr (assoc selected-symbol name-and-pos)))
+      (cond
+       ((overlayp position)
+        (goto-char (overlay-start position)))
+       (t
+        (goto-char position)))))
+   ((listp symbol-list)
+    (dolist (symbol symbol-list)
+      (let (name position)
+        (cond
+         ((and (listp symbol) (imenu--subalist-p symbol))
+          (prelude-ido-goto-symbol symbol))
+         ((listp symbol)
+          (setq name (car symbol))
+          (setq position (cdr symbol)))
+         ((stringp symbol)
+          (setq name symbol)
+          (setq position
+                (get-text-property 1 'org-imenu-marker symbol))))
+        (unless (or (null position) (null name)
+                    (string= (car imenu--rescan-item) name))
+          (add-to-list 'symbol-names name)
+          (add-to-list 'name-and-pos (cons name position))))))))
+
+;; show the name of the current function definition in the modeline
+(require 'which-func)
+(which-func-mode t)
+
+(defun core-prog-mode-hook ()
+  "Default coding hook, useful with any programming language."
+  (flyspell-prog-mode)
+  ;; keep the whitespace decent all the time
+  (add-hook 'before-save-hook 'whitespace-cleanup nil t))
+
+;; in Emacs 24 programming major modes generally derive
+;; from a common mode named prog-mode
+(add-hook 'prog-mode-hook 'core-prog-mode-hook)
 
 (provide 'core-programming)
--- modules/core-python.el
+++ modules/core-python.el
@@ -33,4 +33,96 @@
 
 ;;; Code:
 
+;; load the python directory files
+;; and set the variable
+(setq python-files-dir (concat core-vendor-dir "python/"))
+(add-to-list 'load-path python-files-dir)
+(add-to-list 'load-path (concat core-vendor-dir "auto-complete.el"))
+;; We never want to edit python bytecode
+(add-to-list 'completion-ignored-extensions ".pyc")
+
+;; enable the python mode
+(setq auto-mode-alist (cons '("\\.py$" . python-mode) auto-mode-alist))
+(setq interpreter-mode-alist (cons '("python" . python-mode) interpreter-mode-alist))
+
+(autoload 'python-mode "python-mode" "Python editing mode." t)
+
+;;(eval-after-load "pymacs"
+;;  '(add-to-list 'pymacs-load-path YOUR-PYMACS-DIRECTORY"))
+(setq pymacs-load-path (append (list (concat python-files-dir "rope/")
+                                     (concat python-files-dir "ropemode/")
+                                     (concat python-files-dir "pymacs/")
+                                     (concat python-files-dir "ropemacs/"))
+                                nil))
+;;enable pymacs
+(require 'python-mode)
+(require 'auto-complete)
+(require 'auto-complete-config)
+
+;; python indentation hooks
+;; TODO: clean this up a bit by hooking into a dispatcher function
+(add-hook 'python-mode-hook
+      (lambda ()
+        (set-variable 'py-indent-offset 4)
+        (set-variable 'py-continuation-offset 0)
+        ;(set-variable 'py-smart-indentation nil)
+        (set-variable 'indent-tabs-mode nil)
+        (define-key py-mode-map (kbd "C-RET") 'newline-and-indent)
+        (local-set-key (kbd "<M-S-iso-lefttab>") 'mahmoud-force-indent)
+        (auto-complete-mode t)
+      ))
+
+(defun mahmoud-force-indent (&optional arg)
+    (interactive "P")
+    (insert-tab arg))
+
+
+(defun reload-pymacs ()
+    (interactive)
+    (pymacs-terminate-services)
+    (setenv "PYMACS_PYTHON"  (concat virtualenv-root "/" virtualenv-workon "/bin/python"))
+    (pymacs-load "ropemacs" "rope-")
+    (setq ropemacs-enable-autoimport 't))
+
+
+;; load virtualenv
+(add-to-list 'load-path (concat core-vendor-dir "virtualenv.el"))
+(require 'virtualenv)
+
+;; load flymake lint runner
+(when (load "flymake" t)
+  (defun flymake-python-lint-init (&optional trigger-type)
+    ;; (print virtualenv-workon (get-buffer "*Messages*"))
+    ;; (print virtualenv-root (get-buffer "*Messages*"))
+    ;; (print buffer-file-name (get-buffer "*Messages*"))
+    ;; (print file-local-variables-alist (get-buffer "*Messages*"))
+    (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+           (local-file (file-relative-name
+                        temp-file
+                        (file-name-directory buffer-file-name)))
+           (venv-path (concat virtualenv-root "/" virtualenv-workon))
+           (options (when trigger-type (list "--trigger-type" trigger-type))))
+      (if (and virtualenv-workon (not (string= virtualenv-workon " Virtualenv")))
+          (if (listp options)
+              (progn (push (concat "--virtualenv=" venv-path) options))
+            (let (options (list (concat "--virtualenv=" venv-path))))))
+      (list (concat (getenv "HOME") "/bin/flymake-python/pyflymake.py") (append options (list local-file)))))
+
+  (add-to-list 'flymake-allowed-file-name-masks '("\\.py\\'" flymake-python-lint-init))
+  (add-to-list 'flymake-allowed-file-name-masks '("\\.wsgi\\'" flymake-python-lint-init)))
+
+;; (defadvice flymake-python-lint-init (around flymake-python-lint-init-around)
+;;   "Sets the virtual environment if applicable :)"
+;;   (progn (print virtualenv-mode-name (get-buffer "*Messages*"))
+;;          (print buffer-file-name (get-buffer "*Messages*"))
+;;          (print file-local-variables-alist (get-buffer "*Messages*"))
+;;          ;;(print (buffer-local-variables) (get-buffer "*Messages*"))
+;;          ad-do-it))
+
+;; add python hook when in python to activate flymake lint
+(add-hook 'python-mode-hook (lambda() (flymake-mode t)))
+
+(add-hook 'find-file-hook 'flymake-find-file-hook)
+
 (provide 'core-python)
